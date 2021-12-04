32adee4e2baee3bdcc4b0b768e87c33b6e6cc869
ethanmac <ethanmac@mac.com>
2021-12-05 00:11:14 +0800

    update

--- x_dataset_tool.py
+++ x_dataset_tool.py
@@ -3,8 +3,16 @@ import sys
 from dataset_tool import execute_cmdline
 
 def create_argv():
-    sys.argv = "x_dataset_tool.py create_minstrgb"
+
+    tfrecord_dir = "ds_train"
+    src_dir = "ds_src"
+    num_images = 5000
+
+    cmd = "x_dataset_tool.py create_mnistrgb {} {} --num_images {}".format(tfrecord_dir, src_dir, num_images)
+
+    cmd = cmd.strip()
+    return cmd.split(" ")
 
 if __name__ == "__main__":
-    create_argv()
+    sys.argv = create_argv()
     execute_cmdline(sys.argv)
