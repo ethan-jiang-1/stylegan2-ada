031d78835e81a789c21139ae9f1f3366cbcc16c7
ethanmac <ethanmac@mac.com>
2021-12-05 12:28:47 +0800

    update

--- x_dataset_tool.py
+++ x_dataset_tool.py
@@ -2,7 +2,7 @@ import sys
 
 from dataset_tool import execute_cmdline
 
-def create_argv():
+def create_argv_minstrgb():
 
     tfrecord_dir = "ds_train"
     src_dir = "ds_src"
@@ -13,6 +13,20 @@ def create_argv():
     cmd = cmd.strip()
     return cmd.split(" ")
 
+def create_arg_lilei():
+
+    tfrecord_dir = "ds_train_lilei"
+    src_dir = "ds_chunk_lilei"
+    #num_images = 5000
+
+    cmd = "x_dataset_tool.py create_from_images {} {}".format(tfrecord_dir, src_dir)
+
+    cmd = cmd.strip()
+    return cmd.split(" ")
+
 if __name__ == "__main__":
-    sys.argv = create_argv()
+    #sys.argv = create_argv_minstrgb()
+    sys.argv = create_arg_lilei()
+
     execute_cmdline(sys.argv)
+
